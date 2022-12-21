16821bcb785cf967f6b02b27f2b56d813ebd423b
ymc182 <ymc182@gmail.com>
2022-12-21 18:54:10 +1100

    skills icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,17 @@
 import { useEffect } from "react";
 import { Footer, NavBar } from "./components";
-import { Projects, Home, About, Skills, Header } from "./pages";
+import { Projects, Home, About, Header, Works } from "./pages";
 
 function App() {
-	/* 	useEffect(() => {
-		console.log("check");
-		var docWidth = document.documentElement.offsetWidth;
-
-		[].forEach.call(document.querySelectorAll("*"), function (el) {
-			if (el.offsetWidth > docWidth) {
-				console.log(el, el.offsetWidth, ">", docWidth);
-			}
-		});
-	}, []); */
-
 	return (
 		<div>
 			<NavBar />
 			<Header />
 			<Projects />
 			<About />
+			<Works />
 
-			{/* 	<Skills />
-
-			<Footer /> */}
+			{/* 		<Footer /> */}
 		</div>
 	);
 }
--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -5,6 +5,7 @@ import useSanity from "../../hooks/useSanity";
 import { urlFor } from "../../lib/sanity";
 import AppWrapper from "../../Wrapper/AppWrapper";
 import { IAboutSchema } from "../../constants/types";
+import { DevIcons } from "../../constants";
 
 function About() {
 	const { about } = useSanity();
@@ -44,9 +45,47 @@ function About() {
 						);
 					})}
 				</div>
+				<SkillsSection />
 			</div>
 		</>
 	);
 }
 
 export default AppWrapper(About, "Experience", "");
+
+function SkillsSection() {
+	return (
+		<div className="app__skills">
+			<h2 className="head-text">
+				<span>Skills</span>
+			</h2>
+			<div className="app__skills-list">
+				{DevIcons.map((icon, index) => {
+					return (
+						<motion.div
+							initial={{
+								opacity: 0,
+							}}
+							whileInView={{
+								y: [-10, 0],
+								opacity: 1,
+							}}
+							whileHover={{
+								scale: 1.1,
+							}}
+							transition={{
+								delay: index * 0.1,
+								duration: 0.5,
+								type: "tween",
+							}}
+							className="app__skills-item"
+							key={`${icon}-${index}`}
+						>
+							<i className={icon} />
+						</motion.div>
+					);
+				})}
+			</div>
+		</div>
+	);
+}
